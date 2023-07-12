c1218f0f6781139d963eeb7bdf2106cf96214425
ec50n9 <shelloworld@qq.com>
2023-07-12 14:10:18 +0800

    去除多余代码

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import "./main.css";
 // 创建 #ec_window 元素
 function injectEcWindow() {
   console.log("--- injectEcWindow start ---");
+
   let ecWindow = document.createElement("div");
   ecWindow.id = "ec-window";
   document.body.appendChild(ecWindow);
+  
   console.log("--- injectEcWindow end ---");
 }
 injectEcWindow();
@@ -17,10 +19,6 @@ function initEcWindow() {
 
   const { createApp, h } = Vue;
   createApp({
-    provide: {
-      title: "EcWindow",
-      activated: false,
-    },
     render() {
       return h(App);
     },
